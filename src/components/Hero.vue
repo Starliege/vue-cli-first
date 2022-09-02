0333ec2ec447660d3b6bb33e85a78a8c3383a821
Starliege <urrianif@gmail.com>
2022-09-02 15:31:06 +0200

    done

---
+++ src/components/Hero.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="jumbo">
+      <div class="title">Jumbone</div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Dis parturient montes
+        nascetur ridiculus. Placerat orci nulla pellentesque dignissim enim sit
+        amet venenatis. Purus in mollis nunc sed id semper risus in hendrerit. Mi
+        quis hendrerit dolor magna eget. Sem et tortor consequat id porta nibh. A
+        condimentum vitae sapien pellentesque habitant morbi tristique. Augue
+        lacus viverra vitae congue eu consequat ac felis. Nullam eget felis eget
+        nunc lobortis mattis aliquam faucibus. Amet consectetur adipiscing elit
+        duis tristique sollicitudin nibh sit.
+      </p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HeroTit",
+  };
+  </script>
+  
+  <style scoped>
+   .jumbo {
+    padding: 100px 200px;
+    background: #7a1111;
+    box-shadow: 0 50px 100px #000;
+    text-shadow: 0 1px 0 #000;
+    border-radius: 20px;
+    width: 80%;
+    margin: 0 auto;
+    margin-top: 30px;
+  }
+  
+  .title {
+    font-size: 4rem;
+    font-weight: 800;
+    margin-bottom: 30px;
+  }
+  
+  p {
+    font-size: 1.2rem;
+    line-height: 35px;
+  }
+  </style>
---
+++ src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="top">
+      <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "TitleTit",
+  };
+  </script>
+  
+  <style scoped>
+  .top {
+    padding: 15px 50px;
+    background: rgba(0, 0, 0, 0.3);
+    text-align: center;
+    backdrop-filter: blur(7px);
+    color: #fff;
+  }
+  </style>
